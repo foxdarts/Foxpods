2fa6302517ebcf682f7a9f4c991786582c862a70
foxdarts <davidtfinger@gmail.com>
2023-02-12 14:19:15 -0500

    added item and texture folders for storage.

--- src/main/java/com/foxdarts/foxpods/FoxPods.java
+++ src/main/java/com/foxdarts/foxpods/FoxPods.java
@@ -1,6 +1,7 @@
 package com.foxdarts.foxpods;
 
 
+import com.foxdarts.foxpods.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +23,8 @@ public class FoxPods {
     {
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 

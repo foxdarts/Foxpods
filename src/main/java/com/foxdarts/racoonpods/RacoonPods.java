0c73874aca6e0865c90a7bf6cd7839195189c2b0
foxdarts <davidtfinger@gmail.com>
2023-02-12 16:11:25 -0500

    added files and folders to create items and blocks for racoonpods

--- src/main/java/com/foxdarts/racoonpods/RacoonPods.java
+++ src/main/java/com/foxdarts/racoonpods/RacoonPods.java
@@ -1,9 +1,12 @@
 package com.foxdarts.racoonpods;
 
 
+import com.foxdarts.racoonpods.item.ModCreativeModeTabs;
 import com.foxdarts.racoonpods.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,15 +14,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(racoonpods.MOD_ID)
-public class racoonpods {
+@Mod(RacoonPods.MOD_ID)
+public class RacoonPods {
 
     public static final String MOD_ID = "racoonpods";
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
     //important notes!
-    public racoonpods()
+    public RacoonPods()
     {
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -35,6 +38,28 @@ public class racoonpods {
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
+
+
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS)
+        {
+
+            event.accept(ModItems.RACOON_POUCH);
+            event.accept(ModItems.BIG_RACOON_POUCH);
+
+        }
+
+        if(event.getTab() == ModCreativeModeTabs.RACOON_PODS_TAB)
+        {
+
+            event.accept(ModItems.RACOON_POUCH);
+            event.accept(ModItems.BIG_RACOON_POUCH);
+
+        }
 
 
     }
